6a57c1a46e3ab35ac78b7b9d2163ff52052e1d68
phoogerheide <p.hoogerheide@gmail.com>
2016-04-18 20:27:18 +0200

    total makeover. again.

--- myScript.js
+++
@@ -1,9 +0,0 @@
-    function onmousebtn(btn){
-        document.getElementById(btn).style.borderColor = "#A58322";
-        document.getElementById(btn).style.background = "#bbb";
-    };
-
-    function onmsleave(btn){
-        document.getElementById(btn).style.borderColor = "#855300";
-        document.getElementById(btn).style.background = "#aaa";
-    };
