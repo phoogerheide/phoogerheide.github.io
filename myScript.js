05432085e4e8ca379781cccc9b51f9af70d6bf2a
phoogerheide <p.hoogerheide@gmail.com>
2016-04-18 20:28:16 +0200

    NOPE

---
+++ myScript.js
@@ -0,0 +1,9 @@
+    function onmousebtn(btn){
+        document.getElementById(btn).style.borderColor = "#0044c5";
+        document.getElementById(btn).style.background = "#bbb";
+    };
+
+    function onmsleave(btn){
+        document.getElementById(btn).style.borderColor = "#004485";
+        document.getElementById(btn).style.background = "#aaa";
+    };
