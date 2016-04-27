e0935dce7adbe4fcf41b38688c706bd4fd3dd30c
phoogerheide <p.hoogerheide@gmail.com>
2016-04-27 19:42:53 +0200

    testing css animations

--- myScript.js
+++ myScript.js
@@ -1,9 +1,13 @@
-    function onmousebtn(btn){
-        document.getElementById(btn).style.borderColor = "#0044c5";
-        document.getElementById(btn).style.background = "#bbb";
-    };
-
-    function onmsleave(btn){
-        document.getElementById(btn).style.borderColor = "#004485";
-        document.getElementById(btn).style.background = "#aaa";
-    };
+    // function onmousebtn(btn){
+    //     document.getElementById(btn).style.borderColor = "#000000";
+    //     document.getElementById(btn).style.background = "#ccc";
+    //     document.getElementById(btn).style.height = "39px";
+    //
+    // };
+    //
+    // function onmsleave(btn){
+    //     document.getElementById(btn).style.borderColor = "#3f3f3f";
+    //     document.getElementById(btn).style.background = "#0f0f0f";
+    //     document.getElementById(btn).style.height = "40px";
+    //     document.getElementById(btn).style.fontSize = "14px";
+    // };
